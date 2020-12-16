458678334a7c103edc72bc2eb458d64037e22ca1
Omar Flórez <omarflorez@blacksip.com>
2020-12-16 15:21:45 -0500

    tag manager added

--- components/meta.js
+++ components/meta.js
@@ -37,6 +37,13 @@ export default function Meta() {
         content={`Sitio web de Omar Flórez`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <script dangerouslySetInnerHTML={{ __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KFSGCMS');
+      ` }} />
     </Head>
   )
 }
