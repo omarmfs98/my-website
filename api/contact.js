ded26dcca1485c3cd098636fb04366b4f33bd7c2
Omar Flórez <hello@omarflorez.me>
2021-04-24 15:02:48 -0500

    contact test

---
+++ api/contact.js
@@ -0,0 +1,26 @@
+import sendinblue from "../../lib/sendinblue";
+
+export default async (req, res) => {    
+    const { email, name, message } = req.body;
+    let sendSmtpEmail = {
+        to: [{
+            email
+        }],
+        templateId: 1,
+        params: {
+            name,
+            email
+        },
+        message: message,
+    };
+    await sendinblue(sendSmtpEmail).then(function(data) {
+        res.status(200).json({
+            message: 'Email sended successfully',
+            data
+        });
+    }, function(error) {
+        res.status(error.status).json({
+            error
+        })
+    });
+}
