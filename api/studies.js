64e6233b6e454caf37822d4111070812254a2866
Omar Flórez <hello@omarflorez.me>
2021-04-24 00:58:17 -0500

    fix

--- api/studies.js
+++ api/studies.js
@@ -1,3 +1,5 @@
+import NextCors from 'nextjs-cors';
+
 const typeStudies = [
     { id: 1, name: "Pregrado" }
 ];
@@ -10,7 +12,13 @@ const searchType = (arr, id) => {
     return arr.find(type => type.id === id);
 }
 
-export default (req, res) => {    
+export default (req, res) => {
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET'],
+        origin: '*',
+        optionsSuccessStatus: 200
+    });
     res.status(200).json([    
         { id: 1, name: "Systems Engineer", start: "2015-02-09", end: "2021-06-01", type_study: searchType(typeStudies, 1), college: searchType(typeColleges, 1) }
     ])
