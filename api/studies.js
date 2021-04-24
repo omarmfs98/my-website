36b3f13ce2765fb9d905c2d1a2a0751bcf7e6ad0
Omar Flórez <hello@omarflorez.me>
2021-04-24 00:43:59 -0500

    fix api

---
+++ api/studies.js
@@ -0,0 +1,17 @@
+const typeStudies = [
+    { id: 1, name: "Pregrado" }
+];
+
+const typeColleges = [
+    { id: 1, name: "University of Cordoba" }
+];
+
+const searchType = (arr, id) => {
+    return arr.find(type => type.id === id);
+}
+
+export default (req, res) => {    
+    res.status(200).json([    
+        { id: 1, name: "Systems Engineer", start: "2015-02-09", end: "2021-06-01", type_study: searchType(typeStudies, 1), college: searchType(typeColleges, 1) }
+    ])
+}
