ad0963927b75a53973a20fede78732d426d956b4
Omar Flórez <hello@omarflorez.me>
2021-06-30 01:10:10 -0500

    Update experiences.js

--- api/experiences.js
+++ api/experiences.js
@@ -3,7 +3,8 @@ import NextCors from 'nextjs-cors';
 const typeEnterprises = [
     { id: 1, name: "AXYZ" },
     { id: 2, name: "SEGRITH CORP" },
-    { id: 3, name: "BlackSip" }
+    { id: 3, name: "BlackSip" },
+    { id: 4, name: "Global66" }
 ];
 const searchType = (id) => {
     return typeEnterprises.find(type => type.id === id);
@@ -19,6 +20,7 @@ export default async (req, res) => {
     res.status(200).json([    
         { id: 1, name: "Frontend Developer", start: "2017-08-01", end: "2018-04-01", description: "", enterprise: searchType(1) },
         { id: 2, name: "Frontend Developer", start: "2018-04-01", end: "2020-07-30", description: "", enterprise: searchType(2) },
-        { id: 3, name: "Frontend Developer", start: "2020-09-02", end: "2021-06-28", description: "", enterprise: searchType(3) }
+        { id: 3, name: "Frontend Developer", start: "2020-09-02", end: "2021-06-28", description: "", enterprise: searchType(3) },
+        { id: 3, name: "Frontend Developer", start: "2021-06-05", end: null, description: "", enterprise: searchType(4) }
     ])
 }
