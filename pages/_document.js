ba793192e1de40ec4a930f3105b35f850869acf1
Omar Flórez <hello@omarflorez.me>
2023-09-14 14:56:27 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,8 @@ export default class MyDocument extends Document {
           <noscript dangerouslySetInnerHTML={{ __html: `
             <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFSGCMS"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1076996290197435"
+     crossorigin="anonymous"></script>
           ` }} />
         </body>
       </Html>
