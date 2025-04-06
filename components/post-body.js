328ef4c7009738ecf98ec81d1bfc22ba9940a8a7
Omar Florez <omar.florez@global66.com>
2025-04-06 12:51:25 -0500

    feat: Enhance PostBody component and update dependencies

--- components/post-body.js
+++ components/post-body.js
@@ -1,5 +1,7 @@
 import { getMDXComponent } from 'mdx-bundler/client'
 import { useMemo } from 'react'
+import Giscus from '@giscus/react'
+
 
 export default function PostBody({ code }) {
   const Component = useMemo(() => getMDXComponent(code), [code])
@@ -7,6 +9,23 @@ export default function PostBody({ code }) {
   return (
     <div className="max-w-2xl mx-auto prose prose-custom">
       <Component />
+      <div className="mt-16">
+        <Giscus
+          repo="omarmfs98/my-website"
+          repoId="MDEwOlJlcG9zaXRvcnkyODEwMjUyNTc="
+          category="Ideas"
+          categoryId="DIC_kwDOEMAa6c4Co1oP"
+          mapping="pathname"
+          strict="0"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="bottom"
+          theme="light_tritanopia"
+          lang="en"
+          crossorigin="anonymous"
+          loading="lazy"
+        />
+      </div>
     </div>
   )
 }
